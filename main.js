3d2bd50f1992fb4f49c8f5d1b309c6767f952128
SARAHNAJAM <Sarahawan786.sa@gmail.com>
2024-05-09 17:00:09 +0500

    Student Management System

---
+++ main.js
@@ -0,0 +1,83 @@
+// Student management system
+import inquirer from "inquirer";
+const randomNumber = Math.floor(10000 + Math.random() * 90000);
+let myBalance = 0;
+let answer = await inquirer.prompt([
+    {
+        type: "input",
+        name: "student",
+        message: "Enter Student Name",
+        validate: function (value) {
+            if (value.trim() !== "") {
+                return true;
+            }
+            return "Please enter a non-empty value";
+        },
+    },
+    {
+        type: "list",
+        name: "courses",
+        message: "Select the course to enroll",
+        choices: ["MS.office", "HTML", "CSS", "TYPESCRIPT", "PYTHON"]
+    }
+]);
+const tutionFees = {
+    "MS.office": 2000,
+    "HTML": 2000,
+    "CSS": 2000,
+    "TYPESCRIPT": 2000,
+    "PYTHON": 2000
+};
+console.log(`\nTuition Fees: ${tutionFees[answer.courses]}\n`);
+console.log(`Balance: ${myBalance}\n`);
+let paymentType = await inquirer.prompt([
+    {
+        type: "list",
+        name: "paymentType",
+        message: "Select the payment type",
+        choices: ["CASH", "CARD", "EASYPAISA"]
+    },
+    {
+        type: "input",
+        name: "amount",
+        message: "Enter the amount",
+        validate: function (value) {
+            if (value.trim() !== "") {
+                return true;
+            }
+            return "Please enter a non-empty value";
+        }
+    }
+]);
+console.log(`\nYou selected payment method: ${paymentType.paymentType}`);
+const tutionFeesForCourse = tutionFees[answer.courses];
+const paymentAmount = parseFloat(paymentType.amount);
+if (tutionFeesForCourse === paymentAmount) {
+    console.log(`\nYou have paid ${tutionFeesForCourse}`);
+    myBalance -= tutionFeesForCourse;
+    console.log(`\nYour balance is ${myBalance}`);
+    let viewStatusAnswer = await inquirer.prompt([
+        {
+            type: "list",
+            name: "select",
+            message: "Select the action",
+            choices: ["VIEW STATUS", "EXIT"]
+        }
+    ]);
+    if (viewStatusAnswer.select === "VIEW STATUS") {
+        console.log("Status");
+    }
+    console.log(`Student name: ${answer.student}`);
+    console.log(`Student ID: ${randomNumber}`);
+    console.log(`Course: ${answer.courses}`);
+    console.log(`Tuition fees paid: ${paymentAmount}`);
+    console.log(`Balance: ${myBalance}`);
+}
+else {
+    console.log("\nExisting student management system\n");
+}
+if (tutionFeesForCourse !== paymentAmount) {
+    console.log("Invalid amount due to course");
+    myBalance -= paymentAmount;
+    console.log(`\nYour balance is ${myBalance}`);
+}
